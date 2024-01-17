4874e437a2d47c3eb684ef4b9783197203d4b324
mollybeach <mollyjbeach@gmail.com>
2024-01-17 21:14:42 +0000

    // SPDX-License-Identifier: MIT pragma solidity ^0.8.0;  contract RaiseTheStakes {       struct PlayerInfo {         address player;         uint256 ranking;         uint256 rewardPercentage;         uint256 stake;         uint256 items;     }      // Array to store player information     PlayerInfo[] public players;      // Function to add a new player     function addPlayer(         address _player,         uint256 _rewardPercentage,         uint256 _stake,         uint256 _items     ) public {         PlayerInfo memory newPlayer = PlayerInfo({             player: _player,             ranking: 0,             rewardPercentage: _rewardPercentage,             stake: _stake,             items: _items         });          players.push(newPlayer);     }    function setRank(uint256 _playerId, uint256 _ranking) external {       require(msg.sender == players[_playerId].player, "Sender must be players   address");        players[_playerId].ranking = _ranking;   }    function setRewardPercentage(uint256 _playerId, uint256 _rewardPercentage) external {     // require(msg.sender == players[_playerId].player, "Sender must be players   address");;      players[_playerId].rewardPercentage = _rewardPercentage;   }      // Function to get the number of players     function getNumberOfPlayers() public view returns (uint256) {         return players.length;     }      // Function to get player information by index     function getPlayerInfo(uint256 index)         public         view         returns (             address player,             uint256 ranking,             uint256 rewardPercentage,             uint256 stake,             uint256 items         )     {         require(index < players.length, "Index is out of bounds");         PlayerInfo storage playerInfo = players[index];         return (             playerInfo.player,             playerInfo.ranking,             playerInfo.rewardPercentage,             playerInfo.stake,             playerInfo.items         );     } }

--- contract.sol
+++ contract.sol
@@ -1,75 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
-contract RaiseTheStakes {
-
-
-    struct PlayerInfo {
-        address player;
-        uint256 ranking;
-        uint256 rewardPercentage;
-        uint256 stake;
-        uint256 items;
-    }
-
-    // Array to store player information
-    PlayerInfo[] public players;
-
-    // Function to add a new player
-    function addPlayer(
-        address _player,
-        uint256 _rewardPercentage,
-        uint256 _stake,
-        uint256 _items
-    ) public {
-        PlayerInfo memory newPlayer = PlayerInfo({
-            player: _player,
-            ranking: 0,
-            rewardPercentage: _rewardPercentage,
-            stake: _stake,
-            items: _items
-        });
-
-        players.push(newPlayer);
-    }
-
-  function setRank(uint256 _playerId, uint256 _ranking) external {
-      require(msg.sender == players[_playerId].player, "Sender must be players   address");
-
-      players[_playerId].ranking = _ranking;
+contract SimpleStorage {
+  uint256 storedData;
+	// get function
+  function get() public view returns (uint) {
+    return storedData;
   }
 
-  function setRewardPercentage(uint256 _playerId, uint256 _rewardPercentage) external {
-    // require(msg.sender == players[_playerId].player, "Sender must be players   address");;
-
-    players[_playerId].rewardPercentage = _rewardPercentage;
+  function set(uint x) public {
+    storedData = x;
   }
 
-    // Function to get the number of players
-    function getNumberOfPlayers() public view returns (uint256) {
-        return players.length;
-    }
+  function double() public {
+    storedData *= 2;
+  }
+}
 
-    // Function to get player information by index
-    function getPlayerInfo(uint256 index)
-        public
-        view
-        returns (
-            address player,
-            uint256 ranking,
-            uint256 rewardPercentage,
-            uint256 stake,
-            uint256 items
-        )
-    {
-        require(index < players.length, "Index is out of bounds");
-        PlayerInfo storage playerInfo = players[index];
-        return (
-            playerInfo.player,
-            playerInfo.ranking,
-            playerInfo.rewardPercentage,
-            playerInfo.stake,
-            playerInfo.items
-        );
-    }
+contract MathTest {
+	function multiply(uint a, uint b) public pure returns (uint) {
+    return a*b;
+  }
 }
