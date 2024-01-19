b117c1283d48fa3009098670fdbdbf052ffa892b
mollybeach <mollyjbeach@gmail.com>
2024-01-18 18:30:04 -0800

    trying to push

---
+++ client/src/components/App.tsx
@@ -0,0 +1,16 @@
+import { ConnectKitButton } from 'connectkit';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <ConnectKitButton />
+    </div>
+  );
+}
