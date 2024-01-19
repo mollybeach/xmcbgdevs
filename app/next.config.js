a022ed19fe947f1597e3cff6927fbf607ccc9789
mollybeach <mollyjbeach@gmail.com>
2024-01-19 12:25:32 -0800

    update next config

--- app/next.config.js
+++ app/next.config.js
@@ -1,34 +1,7 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-	/**
-	 * Enable static exports for the App Router.
-	 *
-	 * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-	 */
-	output: "export",
+	reactStrictMode: true,
+	
+  }
   
-	/**
-	 * Set base path. This is usually the slug of your repository.
-	 *
-	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-	 */
-	basePath: "/xmcbgdevs",
-  
-	/**
-	 * Disable server-based image optimization. Next.js does not support
-	 * dynamic features with static exports.
-	 *
-	 * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-	 */
-	images: {
-		loader: 'akamai',
-		path: '/_next/',
-	},
-	assetPrefix: './',
-  };
-  
-  module.exports = nextConfig;
+  module.exports = nextConfig
