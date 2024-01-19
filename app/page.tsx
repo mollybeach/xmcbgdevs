baea5b817a7f81018881d816a3f52680a477b8f1
mollybeach <mollyjbeach@gmail.com>
2024-01-19 11:06:01 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import vercel from "../public/vercel.svg";
-import next from "../public/next.svg";
+import vercel from "./public/vercel.svg";
+import next from "./public/next.svg";
 
 export default function Home() {
   return (
