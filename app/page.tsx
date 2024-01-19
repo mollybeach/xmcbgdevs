10f4c1f7f3583469017ee2e2cdc1ceca999676fb
mollybeach <mollyjbeach@gmail.com>
2024-01-19 10:40:09 -0800

    change image sources

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/xmcbgdevs/app/vercel.svg"
+              src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/xmcbgdevs/app/next.svg"
+          src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
