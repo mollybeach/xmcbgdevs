b5dd55932f43633ba0f5f3c376853cab7a74e499
mollybeach <mollyjbeach@gmail.com>
2024-01-19 10:56:18 -0800

    Added svg

--- app/next.config.js
+++ app/next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
 	 *
 	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 	 */
-	basePath: "/xmcbgdevs/",
+	basePath: "/xmcbgdevs",
   
 	/**
 	 * Disable server-based image optimization. Next.js does not support
